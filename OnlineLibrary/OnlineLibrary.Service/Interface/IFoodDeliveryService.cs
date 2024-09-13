87d139868ac27c96fe16227c802c68030252e6eb
milosevska0 <79375858+milosevska0@users.noreply.github.com>
2024-09-13 23:13:05 +0200

    Integrating partner application.

---
+++ OnlineLibrary/OnlineLibrary.Service/Interface/IFoodDeliveryService.cs
@@ -0,0 +1,14 @@
+﻿using OnlineLibrary.Domain.FoodDeliveryDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Service.Interface
+{
+    public interface IFoodDeliveryService
+    {
+        Task<List<Order>> GetAllActiveOrdersAsync();
+    }
+}
